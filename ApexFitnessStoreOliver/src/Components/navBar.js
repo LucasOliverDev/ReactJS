5810c7fcdb0e8e8309a32616c1ccc40ab137aeb5
Lucas Oliver <lucasoliverdev@gmail.com>
2021-03-11 23:32:20 +0000

    DesafioClase3

---
+++ ApexFitnessStoreOliver/src/Components/navBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const NavBar = () => {
+    return <header>
+                <h1>AF Store</h1>
+                    <nav>
+                        <ul>
+                            <li><a href="">Mancuernas</a></li>
+                            <li><a href="">Maquinas</a></li>
+                            <li><a href="">Barras</a></li>
+                            <li><a href="">Discos</a></li>
+                            <li><a href="">Colchonetas</a></li>
+                        </ul>
+                    </nav>
+           </header>
+}
