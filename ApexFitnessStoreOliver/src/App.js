9b79ed5e7db55cae4027d964ec10f8141641c179
Lucas Oliver <lucasoliverdev@gmail.com>
2021-03-18 22:17:10 +0000

    props.greeting

--- ApexFitnessStoreOliver/src/App.js
+++ ApexFitnessStoreOliver/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer/>
+      <ItemListContainer greeting="Acá está el ItemListContainer" />
     </div>
   );
 }
--- ApexFitnessStoreOliver/src/Components/ItemListContainer/index.js
+++ ApexFitnessStoreOliver/src/Components/ItemListContainer/index.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 
-export class ItemListContainer extends Component {
-    render() {
-        return <p>Acá estará el "ItemListContainer"</p>
-    }
+export const ItemListContainer = (props) => {
+    return (
+         <p className="greeting">
+            {props.greeting}
+        </p>
+    )
 }
